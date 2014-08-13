73a7b635363f78448df96f10b4b0be6f7d63a3bd
Adrienne <adriennemueller@users.noreply.github.com>
2014-08-13 08:56:18 -0700

    Rotating Rectangle Generation

---
+++ lp/make_rotating_rect.m
@@ -0,0 +1,37 @@
+function movie = make_rotating_rect( Rwidth, Rheight )
+    rect = rgb(make_rect( Rwidth, Rheight ));
+    movie = make_rotating_movie( rect, 359 );
+end
+
+function movie = make_rotating_movie( I, nFrames )
+    movie = I;
+    step = 360 / (nFrames + 1);
+
+    for angle = step : step : (360 - step)
+        frame = imrotate(I, angle, 'bicubic', 'crop');
+        movie = cat(4, movie, frame);
+    end
+end
+
+function img = make_rect( Rwidth, Rheight )
+    Iwidth =  ceil(sqrt(Rwidth^2 + Rheight^2))+4;
+    Iheight = Iwidth;
+
+    % Define the image size for the background
+    img = false(Iwidth, Iheight);
+
+    % Make the rectangle
+    left   = floor(Iwidth/2  - Rwidth/2);
+    right  = floor(Iwidth/2  + Rwidth/2);
+    top    = floor(Iheight/2 - Rheight/2);
+    bottom = floor(Iheight/2 + Rheight/2);
+
+    img(left:right, top:bottom) = true;
+end
+
+function I = rgb( I )
+    I = double(I);
+    if size(I, 3) == 1
+        I = repmat(I, 1, 1, 3);
+    end
+end
