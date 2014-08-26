6489d8d42a058b86bac8542751c2a4abd970ed64
Adrienne <adriennemueller@users.noreply.github.com>
2014-08-26 11:33:48 -0700

    Code to generate stick-cue.

---
+++ lp/make_rotating_stick.m
@@ -0,0 +1,38 @@
+function movie = make_rotating_stick( Rwidth, Rheight )
+    rect = rgb(make_rect( Rwidth, Rheight ));
+    movie = make_rotating_movie( rect, 359 ); % Use 359 for 1deg steps
+end
+
+function movie = make_rotating_movie( I, nFrames )
+    movie = I;
+    step = 360 / (nFrames + 1);
+
+    for angle = step : step : (360 - step)
+        frame = imrotate(I, angle, 'bicubic', 'crop');
+        imwrite( frame, ['rect_' num2str(round(angle)) '.jpeg'] );
+        movie = cat(4, movie, frame);
+    end
+end
+
+function img = make_rect( Rwidth, Rheight )
+    Iwidth =  (ceil(sqrt(Rwidth^2 + Rheight^2))) *2;
+    Iheight = Iwidth;
+
+    % Define the image size for the background
+    img = false(Iwidth, Iheight);
+
+    % Make the rectangle
+    left   = floor(Iwidth/2  - Rwidth/2);
+    right  = floor(Iwidth/2  + Rwidth/2);
+    top    = floor(Iheight/2);
+    bottom = floor(Iheight/2 + Rheight);
+
+    img(left:right, top:bottom) = true;
+end
+
+function I = rgb( I )
+    I = double(I);
+    if size(I, 3) == 1
+        I = repmat(I, 1, 1, 3);
+    end
+end
