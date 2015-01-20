93d90ccefb43ccfd7c6d8101c996b761149357ca
Adrienne <adrienne.l.mueller@gmail.com>
2015-01-20 15:31:40 -0800

    Cued Blanked LP distractor paradigm - with blocks.

---
+++ lp_block/gen_mobile_dstrctr_conditions_file_cued.m
@@ -0,0 +1,170 @@
+function gen_mobile_dstrctr_conditions_file_cued( filename, b_span, b_overlap, nTrials )
+    path( path, '../' );
+
+    fid = fopen(filename, 'w');
+    
+    textline = generate_condition('Header', 6, 'FID', fid);
+    fprintf(fid,  '%s\n');
+
+	cond = 0;
+
+
+	for i = 1:nTrials
+		tar1_start = randi(36)*5;
+		tar1_end   = randi(36)*5;
+		tar2       = randi(36)*5;
+		
+		if tar1_start == tar1_end
+			continue
+        end
+        
+        tposes = gen_target_positions();
+        t1x = tposes(1); t1y = tposes(2);
+        t2x = tposes(3); t2y = tposes(4);
+        cue_angle = tposes(5);
+        
+		cond = cond + 1;
+		blocks = get_blocks( b_span, b_overlap, tar1_start, tar1_end );
+		s.t_info = 'T1 Change';
+		make_condition(tar1_start, tar1_end, tar2, tar2, t1x, t1y, t2x, t2y, cue_angle, cond, blocks, 1, s, fid);
+		
+		cond = cond + 1;
+		%blocks = get_blocks( b_span, b_overlap, tar1_start, tar1_start );
+		s.t_info = 'Same';
+		make_condition(tar1_start, tar1_start, tar2, tar2, t1x, t1y, t2x, t2y, cue_angle, cond, blocks, 1, s, fid);
+	
+    end
+    % step = 5;
+    % N = 175;
+    
+    % for tar1_start = 0:step:N
+        % for tar1_end = 0:step:N
+            % if tar1_start == tar1_end
+                % continue
+            % end
+            
+            % for tar2_start = 0:step:N
+				
+				% cond = cond + 1;
+				% block = get_blocks( b_span, b_overlap, tar1_start, tar1_start );
+				% s.t_info = 'Same';
+				% make_condition(tar1_start, tar1_start, tar2_start, tar2_start, cond, 1, 2, s, fid);
+				
+				% cond = cond + 1;
+				% blocks = get_blocks( b_span, b_overlap, tar1_start, tar1_end );
+				% s.t_info = 'T1 Change';
+				% make_condition(tar1_start, tar1_end, tar2_start, tar2_start, cond, blocks, 1, s, fid);
+                
+                
+            
+            % end
+        % end
+    % end
+
+    fclose(fid);
+
+end
+
+function blocks = get_blocks( span, overlap, t_s, t_e )
+
+%  |-- span --|
+%  {------[---}------]------------
+%         |---|
+%        overlap
+
+    change = abs(t_s - t_e);
+    change = min([change (180 - change)]);
+    
+        
+    num_blocks = ceil(90/(span - overlap));
+
+    if change == 0
+        blocks = [1:num_blocks];
+        return
+    end
+    
+    blocks = [];
+    for i = num_blocks:-1:1
+        block_start = (span - overlap) * (i - 1);
+        block_end = block_start + span;
+        if (block_start <= change) && (change <= block_end)
+            blocks = [blocks (num_blocks +1 - i)];
+        end
+    end
+   
+    %disp( ['Change: ' num2str(change) ' Blocks: ' num2str(blocks)] );
+end
+
+function make_condition(t1_s, t1_e, t2_s, t2_e, t1x, t1y, t2x, t2y, cue_angle, cond, block, freq, s, fid)
+
+    TaskObject(1).Type = 'Fix';
+    TaskObject(1).Arg{1} = 0;
+    TaskObject(1).Arg{2} = 0;
+
+    TaskObject(2).Type = 'Pic';
+    TaskObject(2).Arg{1} = ['rect_' num2str(t1_s)];
+    TaskObject(2).Arg{2} = t1x;
+    TaskObject(2).Arg{3} = t1y;
+    TaskObject(2).Arg{4} = 150;
+    TaskObject(2).Arg{5} = 150;
+
+    TaskObject(3).Type = 'Pic';
+    TaskObject(3).Arg{1} = ['rect_' num2str(t1_e)];
+    TaskObject(3).Arg{2} = t1x;
+    TaskObject(3).Arg{3} = t1y;
+    TaskObject(3).Arg{4} = 150;
+    TaskObject(3).Arg{5} = 150;
+
+    TaskObject(4).Type = 'Pic';
+    TaskObject(4).Arg{1} = ['rect_' num2str(t2_s)];
+    TaskObject(4).Arg{2} = t2x;
+    TaskObject(4).Arg{3} = t2y;
+    TaskObject(4).Arg{4} = 150;
+    TaskObject(4).Arg{5} = 150;
+
+    TaskObject(5).Type = 'Pic';
+    TaskObject(5).Arg{1} = ['rect_' num2str(t2_e)];
+    TaskObject(5).Arg{2} = t2x;
+    TaskObject(5).Arg{3} = t2y;
+    TaskObject(5).Arg{4} = 150;
+    TaskObject(5).Arg{5} = 150;
+    
+    TaskObject(6).Type = 'Pic';
+    TaskObject(6).Arg{1} = ['cue_' num2str(cue_angle)];
+    TaskObject(6).Arg{2} = 0;
+    TaskObject(6).Arg{3} = 0;
+    TaskObject(6).Arg{4} = 200;
+    TaskObject(6).Arg{5} = 200;
+
+    %disp(['T1: ' num2str(t1_s) ' -> ' num2str(t1_e) '    T2: ' num2str(t2_s) ' -> ' num2str(t2_e) ]);
+    
+    generate_condition(...
+        'Condition', cond, ...
+        'Block', block, ...
+        'Frequency', freq, ...
+        'TimingFile', 'lp_dstrctr_cued_mobile_blank', ...
+        'Info', s, ...
+        'TaskObject', TaskObject, ...
+        'FID', fid);
+
+end
+
+
+function rslt = gen_target_positions()
+    anglelist = [0:45:315];
+
+    %Generate New Positions for Targets
+    angle_selection = randi(8); %Get Random Angle
+    theta = anglelist(angle_selection);
+    cueangle = theta;
+    theta = theta * pi/180;
+
+    radius = 3; %randi(5); %Get Randum Radius between 1 and 5
+    [t1x, t1y] = pol2cart(theta, radius); %Convert to polar coordinates
+    t2x = -t1x; t2y = -t1y;
+
+    rslt = [t1x t1y t2x t2y cueangle];
+end
+
+
+
---
+++ lp_block/lp_dstrctr_cued_mobile_blank.m
@@ -0,0 +1,106 @@
+%lp_dstrctr_cued_mobile_blank (timing script)
+
+% Naming for TaskObjects defined in the conditions file:
+start_spot = 1;
+targ1 = 2;
+targ1new = 3;
+targ2 = 4;
+targ2new = 5;
+cue = 6;
+
+editable( 'reward' );
+
+% Define Time Intervals (in ms):
+wait_press = 1000;
+hold_time = 300;
+blank_time = 20;
+hold_time_postcue = 400;
+wait_release = 1000;
+
+% Define Reward Duration
+reward = 300;
+
+tmpstruct = TrialRecord.CurrentConditionStimulusInfo;
+ 
+if strcmp(tmpstruct{2}.Name, tmpstruct{3}.Name)
+	comb = 1; eventmarker(132); %No Change
+	%disp('Comb = 1, ' 'T1: ' [tmpstruct{2}.Name] ' T1 New: ' [tmpstruct{3}.Name] ' T2: ' [tmpstruct{4}.Name] ' T2 New: ' [tmpstruct{5}.Name] );
+elseif ~strcmp(tmpstruct{2}.Name, tmpstruct{3}.Name) 
+	comb = 2; eventmarker(133); %First Change
+	%disp('Comb = 2, ' 'T1: ' [tmpstruct{2}.Name] ' T1 New: ' [tmpstruct{3}.Name] ' T2: ' [tmpstruct{4}.Name] ' T2 New: ' [tmpstruct{5}.Name]);
+end
+
+
+%%%%%%%%% TASK %%%%%%%%
+
+% Show Fixation Spot, Cues the Beginning of a Trial:
+toggleobject(start_spot, 'eventmarker', 120) % Fixation Spot Shown
+
+% Waits for press
+pressed = eyejoytrack('acquiretouch', 1, [], wait_press); % Here '1' =button/lever index, not the target
+if ~pressed
+    toggleobject(start_spot, 'eventmarker', 125) % Didn't press by end of fixation cue
+    trialerror(1); % Didn't press in time
+    idle(200, [1, 0, 0]); % Red Error Screen
+    return
+end
+
+% Turn on Targets
+toggleobject([targ1 targ2], 'eventmarker', 121);
+
+% Tests lever remains pressed
+held = eyejoytrack('holdtouch', 1, [], hold_time);
+if ~held,
+    toggleobject([start_spot targ1 targ2], 'eventmarker', 126); %Turn off fixation spot and targets
+    trialerror(2); % Released too soon
+    idle(200, [1, 0, 0]); % Red Error Screen
+    return
+end
+
+% Turn on Cue
+toggleobject(cue,'eventmarker', 131); % Cue on
+
+% Keep Lever Pressed while Cue is on
+held = eyejoytrack('holdtouch', 1, [], hold_time_postcue);
+if ~held,
+    toggleobject([start_spot cue targ1 targ2], 'eventmarker', 126); % Turn off cue
+    trialerror(2); % Released too early
+    idle(200, [1, 0, 0]); % Red Error Screen
+    return
+end
+
+%Blank Targets
+toggleobject([targ1 targ2], 'eventmarker',134);
+held = eyejoytrack('holdtouch', 1, [], blank_time);
+if ~held,
+    toggleobject([start_spot cue], 'eventmarker', 135); % Turn off fix spot
+    trialerror(2); % Released too early
+    idle(200, [1, 0, 0]); % Red Error Screen
+    return
+end
+
+% Change targets or distractors (different or the same)
+toggleobject([targ1new targ2new]);
+
+% Wait for release
+released = ~eyejoytrack('holdtouch', 1, [], wait_release);
+
+if (released && (comb == 1))
+    toggleobject([start_spot cue targ1new targ2new], 'eventmarker', 127); %Turn off fixation spot and targets
+    trialerror(3); % Released when should not have
+    idle(1500, [1, 0, 0]); % Red Error Screen
+    return
+end
+
+if (~released && (comb == 2))
+    toggleobject([start_spot cue targ1new targ2new], 'eventmarker', 128); %Turn off fixation spot and targets
+    trialerror(4); % Did not release in time
+    idle(1500, [1, 0, 0]); % Red Error Screen
+    return
+end
+
+toggleobject([start_spot cue targ1new targ2new], 'eventmarker', 124); %Turn off fixation spot and targets
+trialerror(0); % Correct
+
+% Reward
+goodmonkey(reward);
