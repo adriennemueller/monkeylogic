c847d2dfbdc775445bd8d0caba8b109296e65e9b
Adrienne <adrienne.l.mueller@gmail.com>
2015-01-26 16:50:35 -0800

    Added eventmarkers and changed Trialerror numbers. Cleaned up code.

--- mgs/mgs.m
+++ mgs/mgs.m
@@ -1,12 +1,11 @@
 %mgs (timing script)
 
 % This task requires the animal to hold their gaze steady on a central
-% fixation spot while a peripheral cue is flashed, then maintain fixation
-% during a delay period, then make an eye movement to the remembered
+% fixation spot while a peripheral cue is flashed; then maintain fixation
+% during a delay period; then make an eye movement to the remembered
 % location.
 
 editable( 'fix_radius', 'reward_value', 'initial_fix', 'cue_time', 'delay', 'saccade_time', 'hold_target_time' );
-
 
 % give names to the TaskObjects defined in the conditions file:
 fixation_point = 1;
@@ -15,89 +14,96 @@ target = 3;
 
 % define time intervals (in ms):
 wait_for_fix = 1000;
-initial_fix = 250;
+initial_fix = 300;
+cue_time = 80;
+delay = 500;
 max_reaction_time = 500;
-cue_time = 60;
-delay = 300;
 saccade_time = 80;
-hold_target_time = 100;
+hold_target_time = 200;
 
 % fixation window (in degrees):
-fix_radius = 2;
+fix_radius = 1.3;
 
 reward_value = 300;
+
+%%% Trial Errors %%%
+% 0   Correct.
+% 1   No initial fixation.
+% 2   Broke fixation.
+% 3   Did not react.
+% 4  Reacted but did not capture target.
+
+
 
 % TASK:
 
 % initial fixation:
-toggleobject(fixation_point);
+toggleobject(fixation_point, 'eventmarker', 120);
 ontarget = eyejoytrack('acquirefix', fixation_point, fix_radius, wait_for_fix);
 if ~ontarget,
-    trialerror(4); % no fixation
-    toggleobject(fixation_point)
+    trialerror(1); % no fixation
+    toggleobject(fixation_point, 'eventmarker', 121)
     idle(200, [1, 0, 0]); % Red Error Screen
     return
 end
 ontarget = eyejoytrack('holdfix', fixation_point, fix_radius, initial_fix);
 if ~ontarget,
-    trialerror(3); % broke fixation
-    toggleobject(fixation_point)
+    trialerror(2); % broke fixation
+    toggleobject(fixation_point, 'eventmarker', 122)
     idle(200, [1, 0, 0]); % Red Error Screen
     return
 end
 
 
 % cue epoch
-toggleobject(cue); % turn on cue
+toggleobject(cue, 'eventmarker', 123); % turn on cue
 ontarget = eyejoytrack('holdfix', fixation_point, fix_radius, cue_time);
 if ~ontarget,
-    trialerror(3); % broke fixation
-    toggleobject([fixation_point cue])
+    trialerror(2); % broke fixation
+    toggleobject([fixation_point cue], 'eventmarker', 124)
     idle(200, [1, 0, 0]); % Red Error Screen
     return
 end
-toggleobject(cue); % turn off sample
+toggleobject(cue, 'eventmarker', 125); % turn off sample
 
 % delay epoch
 ontarget = eyejoytrack('holdfix', fixation_point, fix_radius, delay);
 if ~ontarget,
-    trialerror(3); % broke fixation
-    toggleobject(fixation_point)
+    trialerror(2); % broke fixation
+    toggleobject(fixation_point, 'eventmarker', 126)
     idle(200, [1, 0, 0]); % Red Error Screen
     return
 end
 
 % choice presentation and response
-toggleobject([fixation_point target]); % simultaneously turns of fix point and displays target & distractor
+toggleobject([fixation_point target], 'eventmarker', 127); % simultaneously turns of fix point and displays target & distractor
 [ontarget rt] = eyejoytrack('holdfix', fixation_point, fix_radius, max_reaction_time); % rt will be used to update the graph on the control screen
 if ontarget, % max_reaction_time has elapsed and is still on fix spot
-    trialerror(1); % no response
-    toggleobject(target)
+    trialerror(3); % no response
+    toggleobject(target, 'eventmarker', 128)
     idle(200, [1, 0, 0]); % Red Error Screen
     return
 end
+
 ontarget = eyejoytrack('acquirefix', target, fix_radius, saccade_time);
 if ~ontarget,
-    trialerror(2); % no or late response (did not land on either the target or distractor)
-    toggleobject(target)
+    trialerror(4); % no or late response (did not land on the target)
+    toggleobject(target, 'eventmarker', 129)
     idle(200, [1, 0, 0]); % Red Error Screen
     return
-elseif ontarget == 2,
-    trialerror(6); % chose the wrong (second) object among the options [target distractor]
-    toggleobject(target)
-    idle(200, [1, 0, 0]); % Red Error Screen
-    return
+
 end
 
 % hold target then reward
 ontarget = eyejoytrack('holdfix', target, fix_radius, hold_target_time);
 if ~ontarget,
-    trialerror(5); % broke fixation
-    toggleobject(target)
+    trialerror(2); % broke fixation
+    toggleobject(target, 'eventmarker', 130)
     idle(200, [1, 0, 0]); % Red Error Screen
     return
 end
-trialerror(0); % correct
-goodmonkey(reward_value); % 50ms of juice x 3
 
-toggleobject(target); %turn off remaining objects
+trialerror(0); % correct
+toggleobject(target, 'eventmarker', 131); %turn off remaining objects
+goodmonkey(reward_value); % juice
+
