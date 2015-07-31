a8eb55db9385735ca40f762a77d8c978491e2007
Adrienne <adrienne.l.mueller@gmail.com>
2015-07-31 11:12:44 -0700

    Changed cue and stimuli generating functions to be green on grey and actually save out the movie.

--- lp/make_rotating_rect.m
+++ lp/make_rotating_rect.m
@@ -2,7 +2,8 @@
 
 function movie = make_rotating_rect( Rwidth, Rheight )
     rect = rgb(make_rect( Rwidth, Rheight ));
-    movie = make_rotating_movie( rect, 71 );
+    rect = colorize_rect( rect, [0.001 0.69 0.001] ); % Green
+    movie = make_rotating_movie( rect, 71 ); %Why 71? Should be 7??
 end
 
 function movie = make_rotating_movie( I, nFrames )
@@ -10,10 +11,16 @@ function movie = make_rotating_movie( I, nFrames )
     step = 360 / (nFrames + 1);
 
     for angle = step : step : (360 - step)
-        frame = imrotate(I, angle, 'bicubic', 'crop');
+        frame = imrotate(I, angle, 'crop', 'bilinear'); %, 'bicubic', 'crop');
+        
+        %Make background grey instead of black
+        frame(frame==0) = 0.55;
+        %frame(frame==1) = 0;     
         imwrite( frame, ['rect_' num2str(round(angle)) '.jpeg'] );
         movie = cat(4, movie, frame);
     end
+    mov = immovie(movie);
+    movie2avi(mov, 'rect.avi', 'compression', 'Cinepak');    
 end
 
 function img = make_rect( Rwidth, Rheight )
@@ -22,6 +29,7 @@ function img = make_rect( Rwidth, Rheight )
 
     % Define the image size for the background
     img = false(Iwidth, Iheight);
+    %img = 0.5 .* img;
 
     % Make the rectangle
     left   = floor(Iwidth/2  - Rwidth/2);
@@ -38,3 +46,25 @@ function I = rgb( I )
         I = repmat(I, 1, 1, 3);
     end
 end
+
+
+function rslt = colorize_rect( rect, Rcolor )
+    
+    [m, n, o] = size( rect );
+    for i = 1:m
+        for j = 1:n
+            if rect(i,j,1) == 1
+                rect(i,j,1) = Rcolor(1);
+            end
+            if rect(i,j,2) == 1
+                rect(i,j,2) = Rcolor(2);
+            end
+            if rect(i,j,3) == 1;
+                rect(i,j,3) = Rcolor(3);
+            end
+        end
+    end
+
+    rslt = rect;
+end
+
--- lp/make_rotating_stick.m
+++ lp/make_rotating_stick.m
@@ -1,7 +1,10 @@
 % Suite of functions to make image files for a cue 'stick'.
+% If centered variable is 1, it will center the stick otherwise it will
+% make one that points out from the center.
 
-function movie = make_rotating_stick( Rwidth, Rheight )
-    rect = rgb(make_rect( Rwidth, Rheight ));
+function movie = make_rotating_stick( Rwidth, Rheight, centered )
+    rect = rgb(make_rect( Rwidth, Rheight, centered ));
+    rect = colorize_rect( rect, [0.001 0.69 0.001] ); % Green
     movie = make_rotating_movie( rect, 7 ); % Use 359 for 1deg steps
 end
 
@@ -11,12 +14,15 @@ function movie = make_rotating_movie( I, nFrames )
 
     for angle = step : step : (360 - step)
         frame = imrotate(I, angle, 'bicubic', 'crop');
-        %imwrite( frame, ['stick_' num2str(round(angle)) '.jpeg'] );
+        frame(frame==0) = 0.55;
+        imwrite( frame, ['cue_' num2str(round(angle)) '.jpeg'] );
         movie = cat(4, movie, frame);
     end
+    mov = immovie(movie);
+    movie2avi(mov, 'cue.avi', 'compression', 'Cinepak');    
 end
 
-function img = make_rect( Rwidth, Rheight )
+function img = make_rect( Rwidth, Rheight, centered )
     Iwidth =  (ceil(sqrt(Rwidth^2 + Rheight^2)) *2) + 4;
     Iheight = Iwidth;
     
@@ -24,10 +30,17 @@ function img = make_rect( Rwidth, Rheight )
     img = false(Iwidth, Iheight);
 
     % Make the rectangle
-    left   = floor(Iwidth/2  - Rwidth/2);
-    right  = floor(Iwidth/2  + Rwidth/2);
-    top    = floor(Iheight/2 -1);
-    bottom = floor(Iheight/2 + Rheight);
+    if centered
+        left   = floor(Iwidth/2  - Rwidth/2);
+        right  = floor(Iwidth/2  + Rwidth/2);
+        top    = floor(Iheight/2 - Rheight/2);
+        bottom = floor(Iheight/2 + Rheight/2);
+    else
+        left   = floor(Iwidth/2  - Rwidth/2);
+        right  = floor(Iwidth/2  + Rwidth/2);
+        top    = floor(Iheight/2 -1);
+        bottom = floor(Iheight/2 + Rheight);
+    end
 
     img(left:right, top:bottom) = true;
 end
@@ -37,4 +50,24 @@ function I = rgb( I )
     if size(I, 3) == 1
         I = repmat(I, [1 1 3]);
     end
+end
+
+function rslt = colorize_rect( rect, Rcolor )
+    
+    [m, n, o] = size( rect );
+    for i = 1:m
+        for j = 1:n
+            if rect(i,j,1) == 1
+                rect(i,j,1) = Rcolor(1);
+            end
+            if rect(i,j,2) == 1
+                rect(i,j,2) = Rcolor(2);
+            end
+            if rect(i,j,3) == 1;
+                rect(i,j,3) = Rcolor(3);
+            end
+        end
+    end
+
+    rslt = rect;
 end
